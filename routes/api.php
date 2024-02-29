d9823e9d40b8d1557d983b80edfe87e7c222cdca
Vsevolod <vsvchp@gmail.com>
2024-03-01 03:28:02 +0400

    main

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,19 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\TokenController;
-use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
-//Route::middleware('auth:sanctum')->group(function () {
-//    Route::post('/create-employee', [UserController::class, 'createEmployee']);
-//    Route::get('/user', [UserController::class, 'getUser']);
-//    Route::delete('/users/{user}', [UserController::class, 'destroy']);
-//});
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
